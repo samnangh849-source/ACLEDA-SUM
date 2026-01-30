4d95f09f849f726cbf26eee3a0fb37981ab6acdd
samnangh849-source <samnangh849@gmail.com>
2026-01-30 15:17:32 +0700

    Update telegram_config.js

--- telegram_config.js
+++ telegram_config.js
@@ -8,11 +8,11 @@ const TELEGRAM_BOT_TOKEN = "7976723335:AAHfuSf-umdTV3kQUd3CbM3Z7xvHGqmHMe0";
 
 // ដាក់ Group ID សម្រាប់ ABA នៅទីនេះ
 // ឧទាហរណ៍: "-1001234567890"
-const TELEGRAM_CHAT_ID_ABA = "YOUR_ABA_GROUP_ID_HERE";
+const TELEGRAM_CHAT_ID_ABA = "-5118943151";
 
 // ដាក់ Group ID សម្រាប់ ACLEDA នៅទីនេះ
 // ឧទាហរណ៍: "-1009876543210"
-const TELEGRAM_CHAT_ID_ACLEDA = "YOUR_ACLEDA_GROUP_ID_HERE";
+const TELEGRAM_CHAT_ID_ACLEDA = "-1002941547085";
 
 // (Optional) ID ទូទៅ ប្រសិនបើរកមិនឃើញជាក់លាក់ខាងលើ
 const TELEGRAM_CHAT_ID = "";
